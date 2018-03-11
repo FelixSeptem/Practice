2d5572181e744e1299d4c1336029711ffe03071e
Felix Septem <whenhow94@qq.com>
2018-03-11 22:52:15 +0800

    add tree travel

--- Data Structure/tree.py
+++ Data Structure/tree.py
@@ -50,7 +50,7 @@ class TreeNode:
         return max(self.maxDepth(tree.left), self.maxDepth(tree.right)) + 1
 
     def mid_travelsal(root):
-        if root.left is None:
+        if root.left is not None:
             mid_travelsal(root.left)
         #访问当前节点
         print(root.value)
@@ -73,4 +73,50 @@ class TreeNode:
 
     
 
-    
+    def front_stack(self, root):
+        """利用堆栈实现树的先序遍历"""
+        if root == None:
+            return
+        myStack = []
+        node = root
+        while node or myStack:
+            while node:                     #从根节点开始，一直找它的左子树
+                print node.elem,
+                myStack.append(node)
+                node = node.lchild
+            node = myStack.pop()            #while结束表示当前节点node为空，即前一个节点没有左子树了
+            node = node.rchild                  #开始查看它的右子树
+
+
+    def middle_stack(self, root):
+        """利用堆栈实现树的中序遍历"""
+        if root == None:
+            return
+        myStack = []
+        node = root
+        while node or myStack:
+            while node:                     #从根节点开始，一直找它的左子树
+                myStack.append(node)
+                node = node.lchild
+            node = myStack.pop()            #while结束表示当前节点node为空，即前一个节点没有左子树了
+            print node.elem,
+            node = node.rchild                  #开始查看它的右子树
+
+
+    def later_stack(self, root):
+        """利用堆栈实现树的后序遍历"""
+        if root == None:
+            return
+        myStack1 = []
+        myStack2 = []
+        node = root
+        myStack1.append(node)
+        while myStack1:                   #这个while循环的功能是找出后序遍历的逆序，存在myStack2里面
+            node = myStack1.pop()
+            if node.lchild:
+                myStack1.append(node.lchild)
+            if node.rchild:
+                myStack1.append(node.rchild)
+            myStack2.append(node)
+        while myStack2:                         #将myStack2中的元素出栈，即为后序遍历次序
+            print myStack2.pop().elem,
