b34c92c2f6bbe247cb64845b348287e4213851f8
Felix Septem <whenhow94@qq.com>
2018-11-10 21:22:21 +0800

    add code

---
+++ LeetCode/25.py
@@ -0,0 +1,176 @@
+# -*- coding:utf-8 -*-  
+'''
+101. Symmetric Tree
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, node):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def helper(root, mirror):
+            if not root and not mirror:
+                return True
+            if root and mirror and root.val == mirror.val:
+                return helper(root.left, mirror.right) and helper(root.right, mirror.left)
+            return False
+        return helper(node, node)
+
+'''
+102. Binary Tree Level Order Traversal
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        ans = [[root.val]]
+        queue = deque([root])
+        while queue:
+            levelans = []
+            for _ in xrange(0, len(queue)):
+                root = queue.popleft()
+                if root.left:
+                    levelans.append(root.left.val)
+                    queue.append(root.left)
+                if root.right:
+                    levelans.append(root.right.val)
+                    queue.append(root.right)
+            if levelans:
+                ans.append(levelans)
+        return ans
+
+'''
+103. Binary Tree Zigzag Level Order Traversal
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        stack = deque([root])
+        ans = []
+        odd = True
+        while stack:
+            level = []
+            for k in xrange(0, len(stack)):
+                top = stack.popleft()
+                if top is None:
+                    continue
+                level.append(top.val)
+                stack.append(top.left)
+                stack.append(top.right)
+            if level:
+                if odd:
+                    ans.append(level)
+                else:
+                    ans.append(level[::-1])
+            odd = not odd
+        return ans
+
+
+'''
+104. Maximum Depth of Binary Tree
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
