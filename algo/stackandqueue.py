eb22a661160b2b70ac61fcc50bce21e063393e74
Felix Septem <whenhow94@qq.com>
2019-02-28 21:32:18 +0800

    add code

--- algo/stackandqueue.py
+++ algo/stackandqueue.py
@@ -127,3 +127,108 @@ def move(n, left, mid, right, from, to):
         print("Move {} from {} to {}".format(n, mid, to))
         part5 = move(n-1, left, mid, right, from, to)
 
+'''
+生成窗口最大值数组
+有一个整数数组arr和一个大小为w的窗口，从数组最左滑动到最右：
+例如，有数组[4,3,5,4,3,3,6,7],窗口大小为3：
+[4 3 5] 4 3 3 6 7 max = 5
+4 [3 5 4] 3 3 6 7 max = 5
+4 3 [5 4 3] 3 6 7 max = 5
+4 3 5 [4 3 3] 6 7 max = 4
+4 3 5 4 [3 3 6] 7 max = 6
+4 3 5 4 3 [3 6 7] max = 7
+input: arr=[4,3,5,4,3,3,6,7], n=3
+output:[5,5,5,4,6,7]
+'''
+def getMaxWindow(arr, w):
+    from collections import deque
+    if not arr || w < 1:
+        return None
+    if len(arr)<w:
+        return max(arr)
+    qmax = deque(maxlen=w)
+    res = []
+    for i in range(len(arr)):
+        while (not qmax) && arr[qmax[-1]]<arr[i]:
+            qmax.pop()
+        qmax.append(i)
+        if qmax[0] == i-w:
+            qmax.popleft()
+        if i>=w-1:
+            res.append(arr[qmax[0]])
+    return res
+
+'''
+求最大子矩阵的大小
+给定一个整形矩阵map,其中值为0和1，求全为1的矩阵区域中，最大矩形区域1的数量。
+例如:
+input:
+1 1 1 0
+output:
+3
+input:
+1 0 1 1
+1 1 1 1
+1 1 1 0
+output:
+6
+'''
+def maxRecFromBottom(height):
+    if not height:
+        return 0
+    maxArea = 0
+    stack = []
+    for i in range(len(height)):
+        while stack && height[i]<=height[stack[-1]]:
+            j = stack.pop()
+            k = -1 if not stack else stack[-1]
+            curArea = (i - k - 1)*height[j]
+            maxArea = max(maxArea, curArea)
+        stack.append(i)
+    while stack:
+        j = stack.pop()
+        k = -1 if not stack else stack[-1]
+        curArea = (len(height) - k - 1)*height[j]
+        maxArea = max(maxArea, curArea)
+    return maxArea
+
+def maxRecSize(map):
+    if (not map) || (not map[0]):
+        return 0
+    maxArea = 0
+    height = []
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            height[j] = 0 if map[i][j]==0 else height[j] + 1
+        maxArea = max(maxRecFromBottom(height), maxArea)
+    return maxArea
+
+'''
+最大值减去最小值小于或等于num的子数组数量
+给定数组arr和整数num，共返回多少子数组满足：
+max(arr[i..j]) - min(arr[i..j])<=num
+'''
+def getNum(arr, num):
+    from collections import deque
+    if not arr:
+        return 0
+    qmin, qmax = deque(), deque()
+    i,j,res = 0,0,0
+    while i<len(arr):
+        while j<len(arr):
+            while qmin && arr[qmin[-1]] > arr[j]:
+                qmin.popleft()
+            qmin.append(j)
+            while qmax && arr[qmax[-1]] <= arr[j]:
+                qmax.popleft()
+            qmax.append(j)
+            if arr[qmax[0]] - arr[qmin[0]] > num:
+                break
+            j++
+        if qmin[0]==i:
+            qmin.popleft()
+        if qmax[0]==i:
+            qmax.popleft()
+        res += j - i
+        i++
+    return res
